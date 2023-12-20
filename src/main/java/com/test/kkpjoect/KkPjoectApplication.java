63eedc82889ebbe628ae3bc12af15ccd6bf45576
STARella01 <seongee2rum@gmail.com>
2023-12-20 15:44:59 +0900

    test

---
+++ src/main/java/com/test/kkpjoect/KkPjoectApplication.java
@@ -0,0 +1,13 @@
+package com.test.kkpjoect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkPjoectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkPjoectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/kkpjoect/KkPjoectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.kkpjoect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkPjoectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
