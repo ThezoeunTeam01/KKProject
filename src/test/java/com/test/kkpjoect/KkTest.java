39d3ccbd6935dd80a2c48923ad3fbebd60bbfc70
case0303 <casebass@naver.com>
2023-12-21 15:11:59 +0900

    테스트

--- src/test/java/com/test/kkpjoect/KkTest.java
+++ src/test/java/com/test/kkpjoect/KkTest.java
@@ -46,7 +46,7 @@ public class KkTest {
         user.setPassWord("123422");
         KkEntity user2 = new KkEntity();
         user2.setUserName("좀 제발 되라고");
-        user2.setPassWord("1234222222223232");
+        user2.setPassWord("12342");
 
         repository.save(user);
         repository.save(user2);
@@ -57,11 +57,36 @@ public class KkTest {
             log.info(forname);//이름
         }
 
-
-
-
-
     }
+    @Test
+    public void testUpdate() {
+        // 사용자 생성 및 저장
+        KkEntity user = new KkEntity();
+        user.setUserName("업데이트 전");
+        user.setPassWord("123422");
+        repository.save(user);
+
+        // 사용자 이름으로 사용자 조회
+        List<KkEntity> userList = repository.findByUserName(user.getUserName());
+        log.info("업데이트 전 사용자 정보: " + userList);
+
+        if (!userList.isEmpty()) {
+            // 사용자의 비밀번호 업데이트
+            KkEntity foundUser = userList.get(0);
+            foundUser.setUserName("업데이트 후");
+
+            // 업데이트된 사용자 엔터티 저장
+            //repository.save(foundUser);
+            log.info("레포지토리 값 : "+repository.save(foundUser));
+            // Optional: 업데이트된 사용자를 검증하기 위해 검색하고 로그에 기록
+            KkEntity updatedUser = repository.findById(foundUser.getUserId()).orElse(null);
+            log.info("업데이트된 사용자 정보: " + updatedUser);
+            log.info("유저 리스트 : " + userList);
+        } else {
+            log.error("해당 사용자를 찾을 수 없습니다.");
+        }
+    }
+
 
 
 
